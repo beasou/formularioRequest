96d1e380216aa480aa87251c8861fa6c705ed289
beasou <beatrizreginasouza1@gmail.com>
2022-04-03 18:17:28 -0300

    correçao json

--- cep.js
+++ cep.js
@@ -1,88 +1,89 @@
-    
-  //AQUI VOU IMPLEMENTAR A HABILITAÇÃO DO 
-function formularioCarregado(){
-    
-}  
+//AQUI VOU IMPLEMENTAR A HABILITAÇÃO DO
+function formularioCarregado() {}
 
-function formularioCarregando(){
-    document.querySelector('.loading').style.visibility = "hidden"
+function formularioCarregando() {
+  document.querySelector(".loading").style.visibility = "hidden";
 }
 
 function limpa_formulário_cep() {
-        //Limpa valores do formulário de cep.
-        document.getElementById('rua').value=("");
-        document.getElementById('bairro').value=("");
-        document.getElementById('cidade').value=("");
-        document.getElementById('estado').value=("");
-        document.getElementById('uf').value=("");
-        document.getElementById('numero').value=("");
-        document.querySelector('.loading').style.visibility = "hidden"
+  //Limpa valores do formulário de cep.
+  document.getElementById("rua").value = "";
+  document.getElementById("bairro").value = "";
+  document.getElementById("cidade").value = "";
+  document.getElementById("estado").value = "";
+  document.getElementById("uf").value = "";
+  document.getElementById("numero").value = "";
+  document.querySelector(".loading").style.visibility = "hidden";
 }
 
 function meu_callback(conteudo) {
-    if (!("erro" in conteudo)) {
-        //Atualiza os campos com os valores.
-        document.getElementById('rua').value=(conteudo.logradouro);
-        document.getElementById('bairro').value=(conteudo.bairro);
-        document.getElementById('cidade').value=(conteudo.localidade);
-        document.getElementById('uf').value=(conteudo.uf);
-    } //end if.
-    else {
-        //CEP não Encontrado.
-        limpa_formulário_cep();
-        alert("CEP não encontrado.");
-    }
+  if (!("erro" in conteudo)) {
+    //Atualiza os campos com os valores.
+    document.getElementById("rua").value = conteudo.logradouro;
+    document.getElementById("bairro").value = conteudo.bairro;
+    document.getElementById("cidade").value = conteudo.localidade;
+    document.getElementById("uf").value = conteudo.uf;
+  } //end if.
+  else {
+    //CEP não Encontrado.
+    limpa_formulário_cep();
+    alert("CEP não encontrado.");
+  }
 }
 
-function obterEndereco(){
-    fetch("https://viacep.com.br/ws/01001000/json/",  {method: 'GET'}).
-    then(function(response){
-        return  response.json()
-    }  ).then(function(info){
-        console.log(info)
+function obterEndereco() {
+  let cep = document.getElementById("cep").value;
+
+  fetch("https://viacep.com.br/ws/" + cep + "/json/", { method: "GET" })
+    .then(function (response) {
+      return response.json();
     })
+    .then(function (conteudo) {
+      //Atualiza os campos com os valores.
+      document.getElementById("rua").value = conteudo.logradouro;
+      document.getElementById("bairro").value = conteudo.bairro;
+      document.getElementById("cidade").value = conteudo.localidade;
+      document.getElementById("uf").value = conteudo.uf;
+    });
 }
-    
+
 function pesquisacep(valor) {
+  //Nova variável "cep" somente com dígitos.
+  var cep = valor.replace(/\D/g, "");
 
-    //Nova variável "cep" somente com dígitos.
-    var cep = valor.replace(/\D/g, '');
+  //Verifica se campo cep possui valor informado.
+  if (cep != "") {
+    //Expressão regular para validar o CEP.
+    var validacep = /^[0-9]{8}$/;
 
-    //Verifica se campo cep possui valor informado.
-    if (cep != "") {
+    //Valida o formato do CEP.
+    if (validacep.test(cep)) {
+      //Preenche os campos com "..." enquanto consulta webservice.
+      document.getElementById("rua").value = "...";
+      document.getElementById("bairro").value = "...";
+      document.getElementById("cidade").value = "...";
+      document.getElementById("uf").value = "...";
+      document.querySelector(".loading").style.visibility = "visible";
+      setInterval(formularioCarregando, 1000);
 
-        //Expressão regular para validar o CEP.
-        var validacep = /^[0-9]{8}$/;
+      //Cria um elemento javascript.
+      var script = document.createElement("script");
 
-        //Valida o formato do CEP.
-        if(validacep.test(cep)) {
+      //Sincroniza com o callback.
+      script.src =
+        "https://viacep.com.br/ws/" + cep + "/json/?callback=meu_callback";
 
-            //Preenche os campos com "..." enquanto consulta webservice.
-            document.getElementById('rua').value="...";
-            document.getElementById('bairro').value="...";
-            document.getElementById('cidade').value="...";
-            document.getElementById('uf').value="...";
-            document.querySelector('.loading').style.visibility = "visible"
-            setInterval(formularioCarregando, 1000)
- 
-
-            //Cria um elemento javascript.
-            var script = document.createElement('script');
-
-            //Sincroniza com o callback.
-            script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
-
-            //Insere script no documento e carrega o conteúdo.
-            document.body.appendChild(script);
-        } //end if.
-        else {
-            //cep é inválido.
-            limpa_formulário_cep();
-            alert("Formato de CEP inválido.");
-        }
+      //Insere script no documento e carrega o conteúdo.
+      document.body.appendChild(script);
     } //end if.
     else {
-        //cep sem valor, limpa formulário.
-        limpa_formulário_cep();
+      //cep é inválido.
+      limpa_formulário_cep();
+      alert("Formato de CEP inválido.");
     }
-};
+  } //end if.
+  else {
+    //cep sem valor, limpa formulário.
+    limpa_formulário_cep();
+  }
+}
--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-function telefone(){
-    if(document.getElementById('yes').checked == true){
-      document.getElementById('tel').disabled = false;
-      document.getElementById('tel').value = "";
-    }
-    if(document.getElementById('no').checked == true){
-      document.getElementById('tel').disabled = true;
-    }
+function telefone() {
+  if (document.getElementById("yes").checked == true) {
+    document.getElementById("tel").hidden = false;
+    document.getElementById("tel").disabled = false;
+    document.getElementById("tel").value = "";
   }
-  
-function checarCadastro(){
-    if(document.getElementById('yes').checked == true){
-        alert("Digite um número de Telefone")
-    }
+  if (document.getElementById("no").checked == true) {
+    document.getElementById("tel").hidden = true;
+  }
+}
+
+function checarCadastro() {
+  if (document.getElementById("yes").checked == true) {
+    alert("Digite um número de Telefone");
+  }
 }
